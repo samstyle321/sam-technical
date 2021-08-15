5272b6a0baec2fb8528a95975ba5c35d29b206d8
samstyle321 <princesyal9@gmail.com>
2021-08-16 00:26:03 +0530

    Contact page

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-100 bg-light text-center footer">
+        <p>ⓒ 2021 SamTechnical. All Rights Reserved | Terms and Conditions</p>
+      </footer>
+    </>
+  );
+};
+export default Footer;
